6405dfadfd86150e6e95baf510971f80b2ccbca4
미니 <cow970814@naver.com>
2022-11-05 09:44:29 +0900

    feat :  add Charger

--- popExample/main.swift
+++ popExample/main.swift
@@ -7,6 +7,29 @@
 struct MacBook {
     var maxWatt: Int = 100
     mutating func chargeBattery(charger: chargeable) {
-        print(maxWatt)
+        let convertedWatt = charger.convert(chargeableWattPerHour: maxWatt)
+        print(maxWatt / convertedWatt)
     }
 }
+
+struct Charger: chargeable {
+    var maximumWattPerHour: WattPerHour
+    
+    func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour {
+        if maximumWattPerHour > chargeableWattPerHour {
+            return maximumWattPerHour
+        }
+        
+        return chargeableWattPerHour
+    }
+}
+
+
+let five = Charger(maximumWattPerHour: 5)
+let iphoneFast = Charger(maximumWattPerHour: 18)
+let ipad = Charger(maximumWattPerHour: 30)
+let macBookCharger = Charger(maximumWattPerHour: 96)
+
+var mac = MacBook()
+
+mac.chargeBattery(charger: macBookCharger)
